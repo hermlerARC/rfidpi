90d3b41e16e9886b73621d518cc3a544740e772a
Dominique Stepek <domstepek@gmail.com>
2019-03-29 22:06:19 -0700

    Changed datetime formatting and added console logs

--- RFID-Checkin/Backend Handling/log.py
+++ RFID-Checkin/Backend Handling/log.py
@@ -9,5 +9,8 @@ class Log(rfidtag.RFIDTag):
     rfidtag.RFIDTag.__init__(self, tag.EPC, tag.Status, tag.Owner, tag.Description, node, tag.Extra)
     self.Timestamp = timestamp
 
+  def __repr__(self):
+    return f"{self.Timestamp},{str(self.Status)},{self.EPC},{self.Owner},{self.Description},{self.Node.Location},{self.Extra}"
+
   def __str__(self):
-    return f"{self.Timestamp}\t{str(self.Status)}\t{self.EPC}\t{self.Owner}\t{self.Description}\t{self.Node.Location}\t{self.Extra}"
+    return f"{self.Timestamp.strftime('%d/%m/%Y %H:%M:%S')}\t{str(self.Status)}\t{self.EPC}\t{self.Owner}\t{self.Description}\t{self.Node.Location}\t{self.Extra}"
